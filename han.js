2eb201c3abc2cd05a0d0d0fa6d2155f38115c4da
lolilukia <lolilukia@163.com>
2018-08-11 00:44:04 +0800

    粘贴的文档，按对勾按钮或者快捷键可以进行全文混排

--- han.js
+++ han.js
@@ -2,10 +2,6 @@ $(function(){
   $('#text').focus();
   // $('#text').height(window.innerHeight);
 
-  var lastTag = 0;
-  var curTag = 0;
-  var pos = [0];
-
   $('#complete').click(function(){mixture();});
   $('#copy').click(function(){copy();});
   $('#quotation').click(function(){quotation();});
@@ -36,7 +32,9 @@ $(function(){
 
   function mixture(){
     var text = $('#text').val();
-
+    text = insert_space(text);
+    $('#text').val(text);
+    $('#text').height($('#text').height() + 120);
   }
 
   function copy(){
@@ -76,83 +74,23 @@ $(function(){
   }
 
   function insert_space(text) {
-    // 英文、數字、符號 ([a-z0-9~!@#&;=_\$\%\^\*\-\+\,\.\/(\\)\?\:\'\"\[\]\(\)])
-
-    // 中文在前
+    console.log(text);
+    // 英文、数字、符号 ([a-z0-9~!@#&;=_\$\%\^\*\-\+\,\.\/(\\)\?\:\'\"\[\]\(\)])
     text = text.replace(/([\u4E00-\u9FA5])([A-Za-z@#&;=_\[\$\%\^\*\-\+\(\/])/ig, '$1 $2');
-
-    // 中文在後
     text = text.replace(/([A-Za-z#!~&;=_\]\,\.\:\?\$\%\^\*\-\+\)\/])([\u4E00-\u9FA5])/ig, '$1 $2');
-
-    // 考慮增加 - + / * 前後的空白
     text = text.replace(/([0-9])([\u4E00-\u9FA5A-Za-z@#&;=_\[\$\%\^\*\-\+\(\/])/ig, '$1 $2');
     text = text.replace(/([\u4E00-\u9FA5A-Za-z#!~&;=_\]\,\.\:\?\$\%\^\*\-\+\)\/])([0-9])/ig, '$1 $2');
-
     return text;
   }
 
   $('#text').keyup(function(e){
-    //win10默认输入法-todo
-    if(e.which == 229){
-
-    }
     if(e.which == 188 || e.which == 190 || e.which == 186 || e.which == 97 || e.which == 111){
-      lastTag = curTag;
-      curTag = $('#text').val().length;
-      var preText = $('#text').val().substring(0, lastTag);
-      var text = $('#text').val().substring(lastTag, curTag);
-      console.log(text);
-      /*
-      var array = text.match(/[A-Za-z\s+]+/g);
-      console.log(array);
-      if(array != null){
-        for(var i = 0; i < array.length; i++){
-          if(array[i].indexOf('\n') != -1) continue;
-          if(text.indexOf(array[i]) + array[i].length + lastTag + 1 == curTag){
-              if(pos[pos.length - 1] == text.indexOf(array[i]) - 1) continue;
-              if(text.indexOf(array[i]) == 0 && lastTag == 0) text = text.replace(array[i], array[i].replace(/(^\s*)|(\s*$)/g, ""));
-              else text = text.replace(array[i], " " + array[i].replace(/(^\s*)|(\s*$)/g, ""));
-          }
-          else if(text.indexOf(array[i]) == 0 && lastTag == 0){
-              text = text.replace(array[i], array[i].replace(/(^\s*)|(\s*$)/g, "") + " ");
-          }
-          else{
-              text = text.replace(array[i], " " + array[i].replace(/(^\s*)|(\s*$)/g, "") + " ");
-          }
-        }
-        $('#text').val(preText + text);
-      }
-      */
+      var text = $('#text').val();
       text = insert_space(text);
-      $('#text').val(preText + text);
-      curTag = $('#text').val().length;
-      pos.push(curTag);
-      console.log(pos);
-    }
-    else if(e.which == 8){
-      var len = pos.length;
-      console.log($('#text').val().length);
-      console.log(pos[len - 1]);
-      if($('#text').val().length < pos[len - 1]){
-        var index = -1;
-        for(var i = 0; i < pos.length; i++){
-          if(pos[i] >= curTag){
-            lastTag = pos[i - 1];
-            index = i;
-            break;
-          }
-        }
-        if(index > 0){
-          pos.splice(index, len - index);
-          curTag = lastTag;
-        }
-        console.log(pos);
-      }
+      $('#text').val(text);
     }
   });
-
   
-
   $('#text').each(function () {
     this.setAttribute('style', 'height:' + (this.scrollHeight) + 'px;overflow-y:hidden;');
   }).on('input', function () {
